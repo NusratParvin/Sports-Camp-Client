658b6d20fa0ef6c5e1b35c2e1eada7bcc35db3fa
NusratParvin <nusrat.official@gmail.com>
2023-06-11 05:55:24 +0400

    dashboard student home

--- src/pages/Dashboard/Student/StudentHome.jsx
+++ src/pages/Dashboard/Student/StudentHome.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import useAuth from '../../../hooks/useAuth';
+import person from '../../../assets/slider/person.png'
 
 const StudentHome = () => {
-    const {user,loading}=useAuth()
-    console.log(user,loading);
+    const { user, loading } = useAuth()
+    console.log(user, loading);
     return (
         <div>
-            <div>Welcome {user.displayName}</div>
+            <div className=' text-center mt-24 text-6xl text-white' >Welcome {user.displayName}
+                <div className='h-54 w-36  mx-auto'>
+                    <img className='w-full h-full object-cover' src={user?.photoURL? user.photoURL:person  } alt="" />
+
+                </div>
+            </div>
         </div>
     );
 };
